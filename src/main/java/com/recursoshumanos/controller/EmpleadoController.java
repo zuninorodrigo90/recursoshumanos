6385bcbb04f2978e53a87f320167d4b134cd6518
Rodrigo <zuninorodrigo90@gmail.com>
2019-10-30 17:35:46 -0300

    JSP de alta empleado vacía

---
+++ src/main/java/com/recursoshumanos/controller/EmpleadoController.java
@@ -0,0 +1,22 @@
+package com.recursoshumanos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.marina.annotation.DescriptionClass;
+
+@DescriptionClass(value = "Empleado")
+@Controller
+public class EmpleadoController {
+
+    /**
+     * Método que carga la pagina de alta empleado.
+     *
+     * @return "altaEmpleado"
+     */
+    @RequestMapping(value = "/empleado/alta")
+    public String cargar() {
+        return "altaEmpleado";
+    }
+
+}
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -59,7 +59,6 @@ body {
 
 <div class="row vertical-offset-100">
 	<div class="col-md-4 ">
-<!-- 	<h3>Sistema Gestión Logística</h3> -->
 		<div class="panel panel-default">
 			<div class="panel-heading">
 			
