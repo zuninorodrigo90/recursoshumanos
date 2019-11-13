b937f6fabafaba1be5502104ed9a38d5456f5975
aye-ahumada <ayelen.ahumada@gmail.com>
2019-11-13 16:19:13 -0300

    finally, decimales

--- src/main/webapp/resources/js/jEmpleado.js
+++ src/main/webapp/resources/js/jEmpleado.js
@@ -1,29 +1,35 @@
 $(document).ready(function () {
 
-//Oculta los fields Aportes y Categoría y los muestra según lo seleccionado en Tipo de empleado
-
-    $("#aportes").hide();
-    $("#categoria").hide();
-
-    $("#tipoDeEmpleado").change(function () {
-        if (($('#tipoDeEmpleado option[value="1"]').is(':selected'))) {
-            $('#categoria').show();
-            $("#aportes").hide();
-
-        } else if (($('#tipoDeEmpleado option[value="2"]').is(':selected'))) {
-            $("#aportes").show();
-            $("#categoria").hide();
-        }
+    //Oculta los fields Aportes y Categoría y los muestra según lo seleccionado en Tipo de empleado
+    
+        $("#aportes").hide();
+        $("#categoria").hide();
+        $("#salario").hide();
+        $("#horasTrabajo").hide();
+    
+        $("#tipoDeEmpleado").change(function () {
+            if (($('#tipoDeEmpleado option[value="1"]').is(':selected'))) {
+                $('#categoria').show();
+                $("#horasTrabajo").show();
+                $("#aportes").hide();
+                $("#salario").hide();
+    
+            } else if (($('#tipoDeEmpleado option[value="2"]').is(':selected'))) {
+                $("#aportes").show();            
+                $("#salario").show();
+                $("#categoria").hide();
+                $("#horasTrabajo").hide();
+            }
+        });
+    
+        //Modal
+    
+        $(".triggerRemove").click(function (e) {
+            e.preventDefault();
+            $("#modalRemove #removeBtn").attr("href",
+                    $(this).attr("href"));
+            $("#modalRemove").modal();
+        });
+    
+    
     });
-
-    //Modal
-
-    $(".triggerRemove").click(function (e) {
-                e.preventDefault();
-                $("#modalRemove #removeBtn").attr("href",
-                        $(this).attr("href"));
-                $("#modalRemove").modal();
-            });
-
-
-});
